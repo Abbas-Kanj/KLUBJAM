955c61ebbdb104b22603ac67c789289d395e629e
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-04-23 13:41:49 +0300

    setting up pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./app/App.tsx";
-import "./index.css";
+import "./common/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
