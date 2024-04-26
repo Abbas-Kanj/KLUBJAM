ef2cef346373f0df92ef597dc64e7faceca02b2e
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-04-26 18:10:42 +0300

    profile header done

--- client/src/pages/Profile/index.tsx
+++ client/src/pages/Profile/index.tsx
@@ -1,5 +1,5 @@
-import bgHeader from "../../assets/Navbar/images/dark-purple-abstract-blur-background-vector.jpg";
-import defaultLogo from "../../assets/Navbar/images/user-profile-circle.svg";
+import bgHeader from "../../assets/Navbar/images/black-blur-background-b21117e1rgm111p3.jpg";
+import defaultLogo from "../../assets/Navbar/images/Ellipse 38.svg";
 import { useState } from "react";
 import Tracks from "./components/Tracks";
 import Albums from "./components/Albums";
@@ -27,14 +27,31 @@ const Profile = () => {
           alt=""
           className="w-full h-full object-center absolute inset-0"
         />
-        <img
-          src={defaultLogo}
-          alt=""
-          className="absolute top-3 right-3 cursor-pointer"
-        />
-        <h1 className="relative mt-[67px] mb-[67px] ml-[35px] font-semibold text-[38px]">
-          Explore
-        </h1>
+        <div className="relative flex ml-[77px]">
+          <img
+            src={defaultLogo}
+            alt=""
+            className="w-[140px] h-[140px] mt-[80px] "
+          />
+          <div className=" flex flex-col ml-[23px] mt-[110px]">
+            <h1 className="font-semibold text-[44px]">John-Dx</h1>
+            <div className="flex mt-[4px] mb-[6px] gap-[28px]">
+              <h2 className="text-[14px] font-medium">John Doe</h2>
+              <h2 className="text-[14px] font-medium">Albania</h2>
+            </div>
+            <p className="font-light text-[13px]">
+              Making heartfelt music, connecting with audiences <br />
+              through inspiring melodies.
+            </p>
+          </div>
+        </div>
+        <div className="flex relative justify-end mr-[35px] gap-[26px]">
+          <h2 className="text-[14px]">6 posts</h2>
+          <h2 className="text-[14px]">11 tracks</h2>
+          <h2 className="text-[14px]">3 albums</h2>
+          <h2 className="text-[14px]">144 followers</h2>
+          <h2 className="text-[14px]">32 following</h2>
+        </div>
       </div>
       <div className="flex items-center justify-center bottom-[13px] left-9 gap-[73px] mt-[16px] mb-[25px]">
         <h2
