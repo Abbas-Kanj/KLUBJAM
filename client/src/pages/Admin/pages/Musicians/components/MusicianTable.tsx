203647a473ae99adf245bea68671e1a7bbc31180
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-04-27 22:29:13 +0300

    making a new table using tailwind

--- client/src/pages/Admin/pages/Musicians/components/MusicianTable.tsx
+++ client/src/pages/Admin/pages/Musicians/components/MusicianTable.tsx
@@ -2,38 +2,7 @@ import React from "react";
 
 const MusicianTable = () => {
   return (
-    <div className="pt-[22px] pb-[22px] pr-[64px] pl-[64px]  min-w-auto h-auto mx-auto ">
-      <table className="flex flex-col w-[1000px] h-auto bg-opacity-70">
-        <thead className="flex gap-[120px] items-center h-[42px] bg-inputBox rounded">
-          <div className="flex justify-between items-center  w-[520px]">
-            <th>ID</th>
-            <th>Fullname</th>
-            <th>Email</th>
-            <th>Status</th>
-            <th>Register Date</th>
-          </div>
-          <div className="flex gap-[50px] max-w-[254px]">
-            <th>Action</th>
-            <th>Action</th>
-            <th>Action</th>
-          </div>
-        </thead>
-        <tbody className="mt-[14px] rounded h-[42px] gap-[110px] flex items-center">
-          <div className="flex justify-between items-center  w-[520px]">
-            <td>1</td>
-            <td>Jhon Doe</td>
-            <td>johndoe@gmail.com</td>
-            <td>Active</td>
-            <td>21/3/2024</td>
-          </div>
-          <div className="flex gap-[20px] max-w-[254px]">
-            <button className="bg-tableUpdateBtn">Update</button>
-            <button className="bg-tableSuspendBtn">Suspend</button>
-            <button className="bg-tableDeleteBtn">Ban</button>
-          </div>
-        </tbody>
-      </table>
-    </div>
+    <div className="pt-[22px] pb-[22px] pr-[64px] pl-[64px]  min-w-auto h-auto mx-auto "></div>
   );
 };
 
