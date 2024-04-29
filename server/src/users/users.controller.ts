014adc281c78f3f64c1f6c01e9c27f630c6afff7
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-04-29 23:41:43 +0300

    added a updateuser in the controller

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { Request, Response } from 'express';
+import { Request, Response, response } from 'express';
 import { JwtAuthGuard } from 'src/authentication/auth.guard';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Get()
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   async getAllUsers(
     @Req() request: Request,
     @Res() response: Response,
@@ -24,6 +35,28 @@ export class UsersController {
       return response.status(500).json({
         status: 'Ok!',
         message: 'Internal Server Error!',
+      });
+    }
+  }
+
+  @Post(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @Res() response: Response,
+  ): Promise<any> {
+    try {
+      const userId = parseInt(id, 10);
+      const result = await this.userService.updateUser(userId, updateUserDto);
+      return response.status(200).json({
+        status: 'Ok!',
+        message: 'Successfully updated user!',
+        result: result,
+      });
+    } catch (err) {
+      return response.status(500).json({
+        status: 'Ok!',
+        message: 'controller error!' + err.message,
       });
     }
   }
