27215b592e695bcf7cd1bf4fe1c9dde55ce9013d
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-04-29 13:51:13 +0300

    piano successfully implemented/ with keydown press

--- client/src/pages/Production/index.tsx
+++ client/src/pages/Production/index.tsx
@@ -16,10 +16,50 @@ import {
 } from "../../tone/instruments/piano";
 
 const Production = () => {
+  window.addEventListener("keydown", (event: KeyboardEvent) => {
+    playNote(event);
+  });
+
   return (
-    <div className="flex flex-col mx-auto items-center justify-center">
-      <div className="w-28 h-28 bg-black" onClick={playC4}>
-        <h1>A</h1>
+    <div className="flex  w-full h-full justify-center mt-10 items-center mx-auto  bg-greyText">
+      <div className="w-8 h-8 bg-black" onClick={playC4}>
+        A
+      </div>
+      <div className="bg-black w-4 h-4" onClick={playDb4}>
+        W
+      </div>
+      <div className="bg-white w-8 h-8" onClick={playD4}>
+        S
+      </div>
+      <div className="bg-black w-4 h-4" onClick={playEb4}>
+        E
+      </div>
+      <div className="bg-white w-8 h-8" onClick={playE4}>
+        D
+      </div>
+      <div className="bg-white w-8 h-8" onClick={playF4}>
+        F
+      </div>
+      <div className="bg-black w-4 h-4" onClick={playGb4}>
+        T
+      </div>
+      <div className="bg-white w-8 h-8" onClick={playG4}>
+        G
+      </div>
+      <div className="bg-black w-4 h-4" onClick={playAb4}>
+        Y
+      </div>
+      <div className="bg-white w-8 h-8" onClick={playA4}>
+        H
+      </div>
+      <div className="bg-black w-4 h-4" onClick={playBb4}>
+        U
+      </div>
+      <div className="bg-white w-8 h-8" onClick={playB4}>
+        J
+      </div>
+      <div className="bg-white w-8 h-8" onClick={playC5}>
+        K
       </div>
     </div>
   );
