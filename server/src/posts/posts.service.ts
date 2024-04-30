2b4aa50f2e32ead212f31ab0b2cb81e8b4777526
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-04-30 13:51:32 +0300

    created a post service

---
+++ server/src/posts/posts.service.ts
@@ -0,0 +1,31 @@
+import { PrismaService } from 'src/prisma.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { Posts } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PostsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createPost(
+    createPostDto: CreatePostDto,
+    userId: number,
+  ): Promise<Posts> {
+    try {
+      const post = await this.prisma.posts.create({
+        data: {
+          ...createPostDto,
+          user: {
+            connect: {
+              id: userId,
+            },
+          },
+        },
+      });
+      return post;
+    } catch (error) {
+      console.error(error); // Log specific Prisma errors
+      throw new Error('An error occurred creating the post'); // Informative error message
+    }
+  }
+}
