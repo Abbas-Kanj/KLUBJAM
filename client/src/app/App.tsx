9d81b8276059b4ca5debde21b8924e82490fc576
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-02 12:44:32 +0300

    added visualizer for step sequencer/ not finished...

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -6,14 +6,16 @@ import Landing from "../pages/Landing";
 import Admin from "../pages/Admin";
 import Moderator from "../pages/Moderator";
 import Production from "../pages/Production";
+import StepSequencer from "../tone/step_sequencer";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<StepSequencer />}></Route>
           <Route path="/Production" element={<Production />}></Route>
-          <Route path="/" element={<Landing />}></Route>
+          <Route path="/Landing" element={<Landing />}></Route>
           <Route path="/Musician/*" element={<Musician />} />
           <Route path="/Admin/*" element={<Admin />} />
           <Route path="/Moderator/*" element={<Moderator />} />
---
+++ client/src/tone/step_sequencer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const StepSequencer = () => {
+  return (
+    <div className="flex mx-auto justify-center items-center w-screen h-screen bg-slate-600">
+      <div className="grid-cols-12 grid-rows-12 w-[500px] h-[400px] rounded bg-orange-700">
+        <div>test</div>
+      </div>
+    </div>
+  );
+};
+
+export default StepSequencer;
