f559d626ea6bbc6e88f353c4bfa019155c7d1219
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-02 23:39:49 +0300

    get all posts controller set

--- server/src/posts/posts.controller.ts
+++ server/src/posts/posts.controller.ts
@@ -5,24 +5,47 @@ import {
   Res,
   Param,
   UploadedFile,
+  Get,
+  Req,
   UseInterceptors,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { Posts } from '@prisma/client';
+import { Request, Response, request } from 'express';
+import path from 'path';
+import * as multer from 'multer';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @Get()
+  async getAllPosts(
+    @Req() request: Request,
+    @Res() response: Response,
+  ): Promise<any> {
+    try {
+      const result = await this.postsService.getAllPosts();
+      return response.status(200).json({
+        status: 'Ok!',
+        message: 'Successfully fetch data!',
+        result: result,
+      });
+    } catch (err) {
+      return response.status(500).json({
+        status: 500,
+        message: 'Internal Server Error!',
+      });
+    }
+  }
+
   @Post(':id')
-  @UseInterceptors(FileInterceptor('image'))
   async createPost(
     @Body() createPostDto: CreatePostDto,
     @Res() response,
     @Param('id') id: string,
-    @UploadedFile() file,
   ): Promise<Posts | { status: number; message: string }> {
     try {
       const userId = parseInt(id, 10);
@@ -30,11 +53,9 @@ export class PostsController {
         throw new Error('Invalid user ID');
       }
 
-      const filename = file;
       const createdPost = await this.postsService.createPost(
         createPostDto,
         userId,
-        filename,
       );
       return response.status(201).json({
         status: 'Ok!',
