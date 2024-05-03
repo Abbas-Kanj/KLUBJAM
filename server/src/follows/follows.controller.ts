d7d91bba36865a705cbe7cd553c696ac7cbf3cfb
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-03 18:53:44 +0300

    created a follow controller for following a specific user

---
+++ server/src/follows/follows.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Controller, Delete, Param, Post, Res } from '@nestjs/common';
+import { Follows } from '@prisma/client';
+import { FollowsService } from './follows.service';
+import { CreateFollowDto } from './dto/create-follow.dto';
+
+@Controller('Follows')
+export class FollowsController {
+  constructor(private readonly followsService: FollowsService) {}
+
+  @Post(':id')
+  async createFollow(
+    @Body() createFollowDto: CreateFollowDto,
+    @Res() response,
+    @Param('id') id: string,
+  ): Promise<Follows | { status: number; message: string }> {
+    try {
+      const follower_id = parseInt(id, 10);
+      if (isNaN(follower_id)) {
+        throw new Error('Invalid post ID');
+      }
+      const following_id = parseInt(String(createFollowDto.following_id), 10);
+
+      if (!following_id) {
+        throw new Error('User ID is required');
+      }
+      const createdFollow = await this.followsService.createFollow(
+        createFollowDto,
+        follower_id,
+        following_id,
+      );
+      return response.status(201).json({
+        status: 'Ok!',
+        message: 'follow created successfully!',
+        result: createdFollow,
+      });
+    } catch (error) {
+      return response.status(500).json({
+        status: 'Error!',
+        message: error.message || 'Controller error',
+      });
+    }
+  }
+}
