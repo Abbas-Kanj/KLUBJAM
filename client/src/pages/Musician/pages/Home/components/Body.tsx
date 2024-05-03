af057e91790c12343a046a61adb1c8a4a6c1e958
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-03 11:46:18 +0300

    declared get request in the body for posts

--- client/src/pages/Musician/pages/Home/components/Body.tsx
+++ client/src/pages/Musician/pages/Home/components/Body.tsx
@@ -1,8 +1,37 @@
+import { useEffect } from "react";
 import Posts from "./Posts";
 import Recommendations from "./Recommendations";
 import Stories from "./Stories";
+import { sendRequest } from "../../../../../core/remote/request";
+import { useAppDispatch } from "../../../../../app/hooks";
+import { setPost } from "../../../../../redux/postsSlice";
 
 const Body = () => {
+  const token = localStorage.getItem("token");
+  const dispatch = useAppDispatch();
+
+  const getPosts = async () => {
+    if (token) {
+      try {
+        const headers = {
+          "Content-Type": "multipart/form-data",
+        };
+        const res = await sendRequest("GET", "/posts", "", headers);
+        if ((res.status = 200)) {
+          console.log("success");
+          // console.log(res.data);
+
+          dispatch(setPost(res.data.result));
+        }
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
   return (
     <div className="flex flex-col relative ">
       <Stories></Stories>
