5ef3127abf5f3af803a5f86589dd54ab8c40e5c6
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-04 00:00:04 +0300

    modified the create like to check if a user already liked a post.

--- server/src/likes/likes.service.ts
+++ server/src/likes/likes.service.ts
@@ -11,6 +11,15 @@ export class LikesService {
     postId: number,
     userId: number,
   ) {
+    const existingLike = await this.prisma.likes.findFirst({
+      where: {
+        post_id: postId,
+        user_id: userId,
+      },
+    });
+    if (existingLike) {
+      throw new Error('User already liked this post');
+    }
     return await this.prisma.likes.create({
       data: {
         post_id: postId,
