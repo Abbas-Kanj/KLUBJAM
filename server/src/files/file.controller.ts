df198f8e21601ee5505bd223c9d1e598e7e878d5
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-03 10:52:58 +0300

    modified the file controller to send back the file path of an image

--- server/src/files/file.controller.ts
+++ server/src/files/file.controller.ts
@@ -3,17 +3,35 @@ import {
   Post,
   UploadedFile,
   UseInterceptors,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('files')
 export class FilesController {
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const randomName = Array(32)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
+          return cb(null, `${randomName}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+  )
   uploadFile(@UploadedFile() file) {
     if (!file) {
-      throw new Error('No file uploaded!');
+      throw new HttpException('No file uploaded!', HttpStatus.BAD_REQUEST);
     }
-    return { message: 'File uploaded successfully' };
+    const filePath = `/uploads/${file.filename}`;
+    return { filePath, status: HttpStatus.OK };
   }
 }
