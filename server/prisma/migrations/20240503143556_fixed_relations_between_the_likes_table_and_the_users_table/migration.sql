7d3dc56657e3fd02d8dcd99a186f1b20c4d57da6
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-03 17:38:51 +0300

    fixed relations between likes and users tables

---
+++ server/prisma/migrations/20240503143556_fixed_relations_between_the_likes_table_and_the_users_table/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "Likes" DROP CONSTRAINT "Likes_user_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Likes" ADD CONSTRAINT "Likes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
