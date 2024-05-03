eb6a79ef6bceded320b5784bdab1d726f5763583
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-03 17:38:28 +0300

    created a likes service for creating a like for a specifc user/ post

---
+++ server/src/likes/likes.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateLikeDto } from './dto/create-like.dto';
+
+@Injectable()
+export class LikesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createLike(
+    createLikeDto: CreateLikeDto,
+    postId: number,
+    userId: number,
+  ) {
+    return await this.prisma.likes.create({
+      data: {
+        post_id: postId,
+        user_id: userId,
+      },
+    });
+  }
+}
