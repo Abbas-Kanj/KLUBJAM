225d95c607398e30b3a921d6e738a4510bb4e4db
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-03 15:43:14 +0300

    created a module for comments/connected main module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './authentication/auth.module';
 import { FileModule } from './files/file.module';
 import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule, FileModule],
+  imports: [UsersModule, PostsModule, CommentsModule, AuthModule, FileModule],
   controllers: [],
   providers: [],
 })
