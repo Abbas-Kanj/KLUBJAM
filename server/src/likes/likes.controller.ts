5bf7b5190f48584949b0f75bb2083ff8759ceb34
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-03 17:38:08 +0300

    created a likes controller for creating a like for a specifc user/ post

---
+++ server/src/likes/likes.controller.ts
@@ -0,0 +1,44 @@
+import { Body, Controller, Param, Post, Res } from '@nestjs/common';
+import { LikesService } from './likes.service';
+import { CreateLikeDto } from './dto/create-like.dto';
+import { response } from 'express';
+import { Likes } from '@prisma/client';
+
+@Controller('likes')
+export class LikesController {
+  constructor(private readonly likesService: LikesService) {}
+
+  @Post(':id')
+  async createLike(
+    @Body() createLikeDto: CreateLikeDto,
+    @Res() response,
+    @Param('id') id: string,
+  ): Promise<Likes | { status: number; message: string }> {
+    try {
+      const postId = parseInt(id, 10);
+      if (isNaN(postId)) {
+        throw new Error('Invalid post ID');
+      }
+      const userId = parseInt(String(createLikeDto.userId), 10);
+
+      if (!userId) {
+        throw new Error('User ID is required');
+      }
+      const createdLike = await this.likesService.createLike(
+        createLikeDto,
+        postId,
+        userId,
+      );
+      return response.status(201).json({
+        status: 'Ok!',
+        message: 'like created successfully!',
+        result: createdLike,
+      });
+    } catch (error) {
+      return response.status(500).json({
+        status: 'Error!',
+        message: error.message || 'Controller error',
+      });
+    }
+  }
+}
