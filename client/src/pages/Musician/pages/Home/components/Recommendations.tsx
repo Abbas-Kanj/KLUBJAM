206d473422bc860d56fee1043e89db4d2e3e9b87
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-04 23:02:07 +0300

    added a request to get recommendatoions for the logged in user, and rendered perfectly

--- client/src/pages/Musician/pages/Home/components/Recommendations.tsx
+++ client/src/pages/Musician/pages/Home/components/Recommendations.tsx
@@ -1,10 +1,35 @@
 import recommendedLogo from "../../../../assets/Home/images/Ellipse 36.svg";
 import post from "../../../../assets/Home/icons/ic_baseline-post-add.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PostPopup from "./PostPopup";
+import { useAppDispatch, useAppSelector } from "../../../../../app/hooks";
+import { sendRequest } from "../../../../../core/remote/request";
+import { setUserRecommendations } from "../../../../../redux/userSlice";
 
 const Recommendations = () => {
   const [openPostPopup, setOpenPostPopup] = useState(false);
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user.user);
+  const recommendations = useAppSelector((state) => state.user.recommendations);
+  const getRecommendations = async () => {
+    try {
+      const headers = {
+        "Content-Type": "multipart/form-data",
+      };
+      const res = await sendRequest("GET", `/follows/${user?.id}`, "", headers);
+      if ((res.status = 200)) {
+        console.log(res);
+
+        dispatch(setUserRecommendations(res.data));
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getRecommendations();
+  }, []);
 
   return (
     <div className="absolute top-0 right-0 mt-[37px] mr-[108px]">
@@ -29,46 +54,27 @@ const Recommendations = () => {
       </div>
       <h2 className="mt-[31px] mb-[16px]">Suggested for you</h2>
       <div>
-        <div className="flex items-center w-[288px] mb-[14px]">
-          <img src={recommendedLogo} alt="" />
-          <div className="flex flex-col justify-between ml-[10px] mr-[130px]">
-            <h2 className="font-medium text-[14px]">john-dx</h2>
-            <h2 className="font-medium text-[12px] text-greyText">john Doe</h2>
+        {recommendations.map((v, i) => (
+          <div
+            key={i}
+            className="flex items-center justify-between w-[288px] mb-[14px]"
+          >
+            <img
+              src={v.profile_picture}
+              alt=""
+              className="max-w-[30px] max-h-[30px] rounded-full"
+            />
+            <div className="flex flex-col justify-between mr-[150px] ">
+              <h2 className="font-medium text-[14px]">{v.username}</h2>
+              <h2 className="font-medium text-[12px] text-greyText">
+                {v.fullname ? null : v.username}
+              </h2>
+            </div>
+            <button className="font-medium text-[14px] text-primary hover:opacity-50">
+              Follow
+            </button>
           </div>
-          <h2 className="font-medium text-[14px] text-primary">Follow</h2>
-        </div>
-        <div className="flex items-center w-[288px] mb-[14px]">
-          <img src={recommendedLogo} alt="" />
-          <div className="flex flex-col justify-between ml-[10px] mr-[130px]">
-            <h2 className="font-medium text-[14px]">john-dx</h2>
-            <h2 className="font-medium text-[12px] text-greyText">john Doe</h2>
-          </div>
-          <h2 className="font-medium text-[14px] text-primary">Follow</h2>
-        </div>
-        <div className="flex items-center w-[288px] mb-[14px]">
-          <img src={recommendedLogo} alt="" />
-          <div className="flex flex-col justify-between ml-[10px] mr-[130px]">
-            <h2 className="font-medium text-[14px]">john-dx</h2>
-            <h2 className="font-medium text-[12px] text-greyText">john Doe</h2>
-          </div>
-          <h2 className="font-medium text-[14px] text-primary">Follow</h2>
-        </div>
-        <div className="flex items-center w-[288px] mb-[14px]">
-          <img src={recommendedLogo} alt="" />
-          <div className="flex flex-col justify-between ml-[10px] mr-[130px]">
-            <h2 className="font-medium text-[14px]">john-dx</h2>
-            <h2 className="font-medium text-[12px] text-greyText">john Doe</h2>
-          </div>
-          <h2 className="font-medium text-[14px] text-primary">Follow</h2>
-        </div>
-        <div className="flex items-center w-[288px] mb-[14px]">
-          <img src={recommendedLogo} alt="" />
-          <div className="flex flex-col justify-between ml-[10px] mr-[130px]">
-            <h2 className="font-medium text-[14px]">john-dx</h2>
-            <h2 className="font-medium text-[12px] text-greyText">john Doe</h2>
-          </div>
-          <h2 className="font-medium text-[14px] text-primary">Follow</h2>
-        </div>
+        ))}
       </div>
     </div>
   );
