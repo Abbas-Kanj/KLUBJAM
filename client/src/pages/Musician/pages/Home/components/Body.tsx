fb69a68763892d6d872965cfc7a8f17a816f35e9
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-04 16:33:15 +0300

    removed the get api from the body, and set it in the posts component, modified the mapping over posts

--- client/src/pages/Musician/pages/Home/components/Body.tsx
+++ client/src/pages/Musician/pages/Home/components/Body.tsx
@@ -1,35 +1,8 @@
-import { useEffect } from "react";
 import Posts from "./Posts";
 import Recommendations from "./Recommendations";
 import Stories from "./Stories";
-import { sendRequest } from "../../../../../core/remote/request";
-import { useAppDispatch, useAppSelector } from "../../../../../app/hooks";
-import { setPost } from "../../../../../redux/postsSlice";
 
 const Body = () => {
-  const token = localStorage.getItem("token");
-  const dispatch = useAppDispatch();
-  const posts = useAppSelector((state) => state.post.posts);
-
-  const getPosts = async () => {
-    if (token) {
-      try {
-        const headers = {
-          "Content-Type": "multipart/form-data",
-        };
-        const res = await sendRequest("GET", "/posts", "", headers);
-        if ((res.status = 200)) {
-          dispatch(setPost(res.data.result));
-        }
-      } catch (error: any) {
-        console.log(error.message);
-      }
-    }
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []);
   return (
     <div className="flex flex-col relative ">
       <Stories></Stories>
--- client/src/pages/Musician/pages/Home/components/Posts.tsx
+++ client/src/pages/Musician/pages/Home/components/Posts.tsx
@@ -1,28 +1,69 @@
-import defaultLogo from "../../../../assets/Home/images/Ellipse 35.svg";
-import post from "../../../../assets/Home/images/Rectangle 80.png";
 import like from "../../../../assets/Home/icons/like.svg";
 import comment from "../../../../assets/Home/icons/comment.svg";
 import share from "../../../../assets/Home/icons/forward.svg";
-import { useAppSelector } from "../../../../../app/hooks";
+import { useAppDispatch, useAppSelector } from "../../../../../app/hooks";
+import CommentsPopup from "./commentsPopup";
+import { useEffect, useState } from "react";
+import { sendRequest } from "../../../../../core/remote/request";
+import { setPosts } from "../../../../../redux/postsSlice";
 
 const Posts = () => {
-  const postsArray = useAppSelector((state) => state.post.posts);
-  const posts = postsArray.flat(); // flatten the array of arrays into a single flat array containing all the posts, allowing you to map through them correctly.
-  console.log(posts);
+  const dispatch = useAppDispatch();
+  const userPosts = useAppSelector((state) => state.user.posts);
+  const posts = useAppSelector((state) => state.post.posts);
+
+  const [openCommentsPopup, setOpenCommentsPopup] = useState(false);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const openCommentPopup = (post: any) => {
+    setSelectedPost(post);
+    setOpenCommentsPopup(true);
+  };
+
+  const getPosts = async () => {
+    try {
+      const headers = {
+        "Content-Type": "multipart/form-data",
+      };
+      const res = await sendRequest("GET", "/posts", "", headers);
+      if ((res.status = 200)) {
+        dispatch(setPosts(res.data.result));
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, [userPosts]);
 
   return (
     <div className="ml-[350px] mt-[48px] mb-[120px]">
+      {openCommentsPopup && (
+        <CommentsPopup
+          post={selectedPost}
+          setOpenCommentsPopup={setOpenCommentsPopup}
+        ></CommentsPopup>
+      )}
       {posts.map((post, i) => (
         <div key={i} className="flex flex-col mb-[30px] ">
           <div className="flex items-center gap-[8px]">
-            <img src={defaultLogo} alt="" />
-            <h2 className="font-medium text-[12px]">01chan_sw - 3h</h2>
+            <img
+              src={post.user.profile_picture}
+              alt=""
+              className="max-w-[40px]  max-h-[40px]"
+            />
+            <h2 className="font-medium text-[12px]">{post.user.username}</h2>
           </div>
           <div
             className="pt-[40px] pb-[40px] pr-[60px] pl-[60px] border-[1px] border-solid border-greyText
          w-[436px] mt-[12px] mb-[12px]"
           >
-            <img src={`http://127.0.0.1:3000${post.post_picture}`} alt="" />
+            <img
+              src={`http://127.0.0.1:3000${post.post_picture}`}
+              alt="nullpic"
+            />
           </div>
           <div className="flex gap-[18px]">
             <img src={like} alt="" className="cursor-pointer" />
@@ -30,21 +71,22 @@ const Posts = () => {
             <img src={share} alt="" className="cursor-pointer" />
           </div>
           <div className="mt-[20px] mb-[20px]">
-            <h2 className="font-medium text-[14px]">748 likes</h2>
+            <h2 className="font-medium text-[14px]">{`${post.likes._count} likes`}</h2>
             <div className="flex gap-[8px]">
-              <h2 className="font-medium text-[14px]">01chan_sw</h2>
-              <h2 className="text-[14px]">
-                Check out this movie that just dropped!
-              </h2>
+              <h2 className="font-medium text-[14px]">{post.user.username}</h2>
+              <h2 className="text-[14px]">{post.caption}</h2>
             </div>
           </div>
           <div>
-            <h2 className="font-medium text-[14px] text-greyText cursor-pointer">
-              View all comments
+            <h2
+              className="font-medium text-[14px] text-greyText cursor-pointer hover:text-white"
+              onClick={() => openCommentPopup(post)}
+            >
+              {`View all ${post.comments._count} comments`}
             </h2>
             <input
               type="text"
-              className="font-medium text-[14px] text-greyText bg-transparent focus:border-transparent cursor-pointer"
+              className="font-medium text-[14px] text-greyText bg-transparent focus:border-transparent cursor-pointer hover:placeholder:text-white"
               placeholder="Add a comment..."
             />
 
