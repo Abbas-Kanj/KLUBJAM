cced9e14cd7696ee02aa25f19ddc2ed6b23cf233
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-06 02:01:55 +0300

    created a track service with get and create a track

---
+++ server/src/tracks/tracks.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { Tracks } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { CreateTrackDto } from './dto/create-track.dto';
+
+@Injectable()
+export class TracksService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllTracks(): Promise<Tracks[]> {
+    return await this.prisma.tracks.findMany();
+  }
+
+  async createTrack(
+    createTrackDto: CreateTrackDto,
+    userId: number,
+  ): Promise<Tracks> {
+    try {
+      const track = await this.prisma.tracks.create({
+        data: {
+          ...createTrackDto,
+          user: {
+            connect: {
+              id: userId,
+            },
+          },
+        },
+      });
+      return track;
+    } catch (error) {
+      console.error(error);
+      throw new Error('An error occurred creating the track');
+    }
+  }
+}
