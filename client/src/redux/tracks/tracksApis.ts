ff1be847ab0bb3434ca94cd379054043e8e0a58c
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-07 02:15:10 +0300

    created a trackApi file to fetch data and store in the track slice

---
+++ client/src/redux/tracks/tracksApis.ts
@@ -0,0 +1,11 @@
+import { sendRequest } from "../../core/remote/request";
+
+export const fetchAllTracksApi = async () => {
+  const res = await sendRequest("GET", `/tracks`, "");
+  if (res.status === 200) {
+    console.log(res.data.result);
+
+    return res.data.result;
+  }
+  return null;
+};
