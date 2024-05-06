936b7fcbf8728361d774a3dd91c5ff4a7db352b4
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-06 22:22:19 +0300

    created a post api file to call get all posts

---
+++ client/src/redux/posts/postsApis.ts
@@ -0,0 +1,9 @@
+import { sendRequest } from "../../core/remote/request";
+
+export const fetchAllPostsApi = async () => {
+  const res = await sendRequest("GET", "/posts", "");
+  if (res.status === 200) {
+    return res.data.result;
+  }
+  return null;
+};
