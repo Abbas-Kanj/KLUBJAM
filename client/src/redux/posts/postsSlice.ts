4933de37b2314008b315374f687bbe3a155e97ad
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-06 22:41:26 +0300

    redefinied fetching all posts logic in the postsSlice

--- client/src/redux/posts/postsSlice.ts
+++ client/src/redux/posts/postsSlice.ts
@@ -1,4 +1,6 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchAllPostsApi } from "./postsApis";
+import { RootState } from "../../app/store";
 
 interface Post {
   id: number;
@@ -11,34 +13,36 @@ interface Post {
 }
 
 interface PostState {
-  posts: Post[];
+  posts: Post[] | null;
 }
 
 const initialState: PostState = {
   posts: [],
 };
 
+export const fetchAllPosts = createAsyncThunk<Post[] | null>(
+  "posts/fetchAllPosts",
+  async (_, { getState }) => {
+    const state = getState() as RootState;
+    const { user } = state.user;
+    if (user) {
+      const result = await fetchAllPostsApi();
+      return result ?? null;
+    }
+    return null;
+  }
+);
+
 const postSlice = createSlice({
   name: "post",
   initialState,
-  reducers: {
-    setPosts: (state, action: PayloadAction<Post[]>) => {
-      action.payload.forEach((newPost) => {
-        const existingPostIndex = state.posts.findIndex(
-          (post) => post.id === newPost.id
-        );
-        if (existingPostIndex !== -1) {
-          state.posts[existingPostIndex] = {
-            ...state.posts[existingPostIndex],
-            ...newPost,
-          };
-        } else {
-          state.posts.push(newPost);
-        }
-      });
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllPosts.fulfilled, (state, action) => {
+      state.posts = action.payload;
+    });
   },
 });
 
-export const { setPosts } = postSlice.actions;
+export const {} = postSlice.actions;
 export default postSlice.reducer;
