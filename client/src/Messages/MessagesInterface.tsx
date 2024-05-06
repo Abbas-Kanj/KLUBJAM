083889eba1ba1789f10b73b4f55c3ca372e73eca
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-06 15:43:03 +0300

    created socket enviroment on the client side to test messages

---
+++ client/src/Messages/MessagesInterface.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import io, { Socket } from "socket.io-client";
+
+const MessagesInterface = () => {
+  const [socket, setSocket] = useState<Socket>();
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const send = (value: string) => {
+    socket?.emit("message", value);
+  };
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:8001");
+    setSocket(newSocket);
+  }, [setSocket]);
+
+  const messageListener = (message: string) => {
+    setMessages([...messages, message]);
+  };
+
+  useEffect(() => {
+    socket?.on("message", messageListener);
+    return () => {
+      socket?.off("message", messageListener);
+    };
+  }, [messageListener]);
+  return <div className=""></div>;
+};
+
+export default MessagesInterface;
