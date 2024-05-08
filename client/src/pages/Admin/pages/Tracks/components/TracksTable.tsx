1735dda37b0a467b1035061975be864b980ce0f6
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-09 02:43:23 +0300

    mapped through tracks slice

--- client/src/pages/Admin/pages/Tracks/components/TracksTable.tsx
+++ client/src/pages/Admin/pages/Tracks/components/TracksTable.tsx
@@ -1,7 +1,10 @@
+import { useAppSelector } from "../../../../../app/hooks";
 import imgIcon from "../../../assets/icons/Group 157.svg";
 import playerIcon from "../../../assets/icons/Vector (1).svg";
 
 const TracksTable = () => {
+  const tracks = useAppSelector((state) => state.track.tracks);
+
   return (
     <div className="mt-[26px] w-[1200px] mx-auto h-auto rounded pt-[22px] pb-[22px] pl-[20px] pr-[20px] bg-tableBackground ">
       <table className="w-[1100px] mx-auto h-auto">
@@ -29,48 +32,45 @@ const TracksTable = () => {
             <th className=" p-3 text-sm font-semibold tracking-wide text-left">
               Play Track
             </th>
-            <th className=" p-3 text-sm font-semibold tracking-wide text-left">
-              Action
-            </th>
-            <th className=" p-3 text-sm font-semibold tracking-wide text-left">
+            <th className=" p-3 text-sm font-semibold tracking-wide text-center">
               Action
             </th>
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-100 ">
-          <tr className="bg-tableRow">
-            <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-              <a href="#" className="font-bold text-black hover:underline">
-                1
-              </a>
-            </td>
-            <td className="p-3 text-sm text-black whitespace-nowrap">
-              John-Dx
-            </td>
-            <td className="p-3 text-sm text-black whitespace-nowrap">Birds</td>
-            <td className="p-3 text-sm text-black whitespace-nowrap">True</td>
-            <td className="p-3 text-sm text-black whitespace-nowrap">
-              Duration
-            </td>
-            <td className="p-3 text-sm text-black whitespace-nowrap cursor-pointer">
-              <img src={imgIcon} alt="" />
-            </td>
-            <td className="p-3 text-sm text-black whitespace-nowrap cursor-pointer">
-              <img src={playerIcon} alt="" />
-            </td>
-
-            <td className="p-3 text-sm text-black whitespace-nowrap">
-              <span className="p-1.5 text-xs font-bold uppercase tracking-wider bg-tableUpdateBtn text-black rounded-lg hover:bg-opacity-30 cursor-pointer">
-                Update
-              </span>
-            </td>
-
-            <td className="p-3 text-sm text-black whitespace-nowrap ">
-              <span className="p-1.5 text-xs font-bold uppercase tracking-wider bg-tableDeleteBtn text-black rounded-lg  cursor-pointer hover:bg-opacity-30 ">
-                Ban
-              </span>
-            </td>
-          </tr>
+          {tracks?.map((track, i) => (
+            <tr key={i} className="bg-tableRow">
+              <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                <a href="#" className="font-bold text-black hover:underline">
+                  {track.id}
+                </a>
+              </td>
+              <td className="p-3 text-sm text-black whitespace-nowrap">
+                {track.user.username}
+              </td>
+              <td className="p-3 text-sm text-black whitespace-nowrap">
+                {track.track_name}
+              </td>
+              <td className="p-3 text-sm text-black whitespace-nowrap">True</td>
+              <td className="p-3 text-sm text-black whitespace-nowrap">
+                {track.duration}
+              </td>
+              <td className="p-3 text-sm text-black whitespace-nowrap cursor-pointer">
+                <img src={imgIcon} alt="" />
+              </td>
+              <td className="p-3 text-sm text-black whitespace-nowrap cursor-pointer">
+                <img src={playerIcon} alt="" />
+              </td>
+              <td className="p-3 text-sm text-black whitespace-nowrap flex justify-evenly">
+                <span className="p-1.5 text-xs  uppercase tracking-wider bg-tableUpdateBtn font-bold text-black rounded-md cursor-pointer hover:bg-opacity-50">
+                  Update
+                </span>
+                <span className="p-1.5 text-xs font-bold uppercase tracking-wider bg-tableDeleteBtn text-black rounded-md  cursor-pointer hover:bg-opacity-30">
+                  Delete
+                </span>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
