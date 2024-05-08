d1b6c6948b2c283aab3ffee6513baf4fb17d2f9b
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-09 01:08:37 +0300

    created a protected admin router

---
+++ client/src/core/route/ProtectedAdmin.tsx
@@ -0,0 +1,46 @@
+import { Outlet, Navigate } from "react-router-dom";
+import Cookies from "universal-cookie";
+import { jwtDecode } from "jwt-decode";
+import { useState, useEffect } from "react";
+
+interface DecodedToken {
+  role: number;
+  exp: any;
+}
+
+const ProtectedAdmin: React.FC = () => {
+  const cookies = new Cookies();
+  const authToken = cookies.get("auth_token");
+
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (authToken) {
+      try {
+        const decodedToken: DecodedToken = jwtDecode(authToken);
+        const userRole = decodedToken.role;
+
+        if (userRole === 1) {
+          setIsAuthorized(true);
+        } else {
+          cookies.remove("auth_token");
+        }
+      } catch (error) {
+        console.error("Error decoding token:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    } else {
+      setIsLoading(false);
+    }
+  }, [authToken]);
+
+  if (isLoading) {
+    return <div>Authenticating...</div>;
+  }
+
+  return isAuthorized ? <Outlet /> : <Navigate to="/" replace />;
+};
+
+export default ProtectedAdmin;
