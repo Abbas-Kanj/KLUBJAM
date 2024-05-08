f04f8ad778be14568b305104187b7db364bbade1
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-09 02:01:49 +0300

    fetched all users data on analytics page

--- client/src/pages/Admin/pages/Analytics/index.tsx
+++ client/src/pages/Admin/pages/Analytics/index.tsx
@@ -1,7 +1,20 @@
 import Header from "./components/Header";
 import AnalyticsCards from "./components/AnalyticsCards";
+import { useEffect } from "react";
+import Cookies from "universal-cookie";
+import { useAppDispatch } from "../../../../app/hooks";
+import { fetchAllUsers } from "../../../../redux/users/usersSlice";
 
 const Analytics = () => {
+  const cookies = new Cookies();
+  const auth_token = cookies.get("auth_token");
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (auth_token) {
+      dispatch(fetchAllUsers());
+    }
+  }, [auth_token, dispatch]);
   return (
     <div className="flex flex-col w-[1316px] bg-backgroundDark">
       <Header></Header>
