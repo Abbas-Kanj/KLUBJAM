88b59b74e238df230c1af10ac5a6fab3278d855f
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-08 18:00:52 +0300

    linked the create projectt button with to open the create project popup

--- client/src/pages/Musician/pages/Workspace/components/SearchBar.tsx
+++ client/src/pages/Musician/pages/Workspace/components/SearchBar.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import downArrow from "../../../../assets/Workspace/icons/chevron-down.svg";
 import note from "../../../../assets/Workspace/icons/note-02.svg";
+import CreateProjectPopup from "./createProjectPopup";
 
 const SearchBar = () => {
+  const [openCreateProjectPopup, setOpenCreateProjectPopup] = useState(false);
+
   return (
     <div className="flex gap-[10px] w-[776px] h-[36px]  mt-[26px]">
+      {openCreateProjectPopup && (
+        <CreateProjectPopup
+          setOpenCreateProjectPopup={setOpenCreateProjectPopup}
+        ></CreateProjectPopup>
+      )}
       <input
         type="text"
         placeholder="Find a project..."
@@ -17,7 +26,12 @@ const SearchBar = () => {
         <h2 className="font-bold text-[14px] text-greyText">Sort</h2>
         <img src={downArrow} alt="" />
       </div>
-      <div className="flex items-center justify-center w-[90px] h-[36px] bg-primary rounded cursor-pointer pt-[6px] pb-[6px] pl-[15px] pr-[15px] gap-[2px]">
+      <div
+        className="flex items-center justify-center w-[90px] h-[36px] bg-primary rounded cursor-pointer pt-[6px] pb-[6px] pl-[15px] pr-[15px] gap-[2px] hover:opacity-70"
+        onClick={() => {
+          setOpenCreateProjectPopup(true);
+        }}
+      >
         <img src={note} alt="" />
         <h2 className="font-bold text-[14px]">Create</h2>
       </div>
