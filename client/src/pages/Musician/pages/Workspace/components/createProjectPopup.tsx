a6e72ec8b7b1f2ac0862504004411345931c32fa
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-08 18:37:50 +0300

    styled the project popup container/ still needs modifying

--- client/src/pages/Musician/pages/Workspace/components/createProjectPopup.tsx
+++ client/src/pages/Musician/pages/Workspace/components/createProjectPopup.tsx
@@ -1,4 +1,4 @@
-import defaultLogo from "../../../../assets/Navbar/images/Ellipse 38.svg";
+import React, { useState } from "react";
 
 interface UpdateMusicianProps {
   setOpenCreateProjectPopup: (open: boolean) => void;
@@ -7,128 +7,128 @@ interface UpdateMusicianProps {
 const CreateProjectPopup: React.FC<UpdateMusicianProps> = ({
   setOpenCreateProjectPopup,
 }) => {
+  const [projectType, setProjectType] = useState("Personal");
+  const [collaboratorInput, setCollaboratorInput] = useState("");
+  const [collaborators, setCollaborators] = useState<string[]>([]);
+
+  const addCollaborator = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (collaboratorInput.trim() !== "") {
+      setCollaborators([...collaborators, collaboratorInput]);
+      setCollaboratorInput("");
+    }
+  };
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-50">
-      <form className="flex flex-col w-[1000px] h-[650px] rounded-xl bg-background">
-        <div className="flex justify-between ">
+      <form
+        className="flex flex-col w-[1000px] h-[650px] rounded-xl bg-background"
+        onSubmit={addCollaborator}
+      >
+        <div className="flex justify-between">
           <p
             className="text-red-500 font-bold cursor-pointer ml-[20px] mt-[10px]"
             onClick={() => setOpenCreateProjectPopup(false)}
           >
             X
           </p>
-          <p className="w-[146px] rounded-xl text-primary  bg-background font-medium text-[14px] pt-[8px] pb-[8px] pr-[25px] pl-[25px] text-center cursor-pointer hover:opacity-50 mt-[10px] mr-[20px]">
-            Save Changes
+          <p className="w-[146px] rounded-xl text-primary bg-background font-medium text-[14px] pt-[8px] pb-[8px] pr-[25px] pl-[25px] text-center cursor-pointer hover:opacity-50 mt-[10px] mr-[20px]">
+            Create Project
           </p>
         </div>
-        <div className="flex flex-col pb-[36px] pl-[108px] pr-[108px] ">
-          <div className="flex flex-col">
-            <h2 className="text-greyText font-semibold text-[16px]">Avatar</h2>
-            <img
-              src={defaultLogo}
-              alt=""
-              className="w-[164px] h-[164px] mb-[10px] mt-[20px]"
-            />
-          </div>
-          <div className="flex gap-[56px]">
-            <div className="flex flex-col gap-[16px]">
-              <div className="flex flex-col gap-[10px]">
-                <label
-                  htmlFor=""
-                  className="font-semibold text-[14px] text-greyText"
-                >
-                  Username
-                </label>
-                <input
-                  type="text"
-                  className="bg-inputBox w-[347px] h-[37px] rounded focus:outline-none p-1"
-                />
-              </div>
-              <div className="flex flex-col gap-[10px]">
-                <label
-                  htmlFor=""
-                  className="font-semibold text-[14px] text-greyText"
-                >
-                  Country
-                </label>
-                <input
-                  type="text"
-                  className="bg-inputBox w-[347px] h-[37px] rounded focus:outline-none p-1"
-                />
-              </div>
+        <div className="flex justify-center mt-[70px] gap-[56px]">
+          <div className="flex flex-col gap-[16px]">
+            <div className="flex flex-col gap-[10px]">
               <label
                 htmlFor=""
                 className="font-semibold text-[14px] text-greyText"
               >
-                Biography
+                Project Name
               </label>
-              <textarea
+              <input
+                type="text"
+                className="bg-inputBox w-[347px] h-[37px] rounded focus:outline-none p-1"
+              />
+            </div>
+            <label
+              htmlFor=""
+              className="font-semibold text-[14px] text-greyText"
+            >
+              Description
+            </label>
+            <textarea
+              name=""
+              id=""
+              className=" bg-inputBox rounded-lg w-[347px] h-[165px] p-2 focus:outline-none"
+            ></textarea>
+          </div>
+          <div className="flex flex-col gap-[16px]">
+            <div className="flex flex-col gap-[10px]">
+              <label
+                htmlFor=""
+                className="font-semibold text-[14px] text-greyText"
+              >
+                Privacy
+              </label>
+              <select
                 name=""
                 id=""
-                className=" bg-inputBox rounded-lg w-[347px] h-[165px] p-2 focus:outline-none"
-              ></textarea>
+                className="bg-inputBox w-[347px] h-[37px] rounded focus:outline-none p-1"
+              >
+                <option>Private</option>
+                <option>Public</option>
+              </select>
             </div>
-            <div className="flex flex-col gap-[16px]">
-              <div className="flex flex-col gap-[10px]">
-                <label
-                  htmlFor=""
-                  className="font-semibold text-[14px] text-greyText"
-                >
-                  Fullname
-                </label>
-                <input
-                  type="text"
-                  className="bg-inputBox w-[347px] h-[37px] rounded focus:outline-none p-1"
-                />
-              </div>
-              <div className="flex flex-col gap-[10px]">
-                <label
-                  htmlFor=""
-                  className="font-semibold text-[14px] text-greyText"
-                >
-                  Email
-                </label>
-                <input
-                  type="text"
-                  className="bg-inputBox w-[347px] h-[37px] rounded focus:outline-none p-1"
-                />
-              </div>
-              <div className="flex flex-col gap-[10px]">
-                <label
-                  htmlFor=""
-                  className="font-semibold text-[14px] text-greyText"
-                >
-                  Phone number
-                </label>
-                <input
-                  type="text"
-                  className="bg-inputBox w-[347px] h-[37px] rounded focus:outline-none p-1"
-                />
-              </div>
-              <div className="flex flex-col gap-[10px]">
-                <label
-                  htmlFor=""
-                  className="font-semibold text-[14px] text-greyText "
-                >
-                  Date of birth
-                </label>
-                <input
-                  type="text"
-                  className="bg-inputBox w-[347px] h-[37px] rounded focus:outline-none p-1"
-                />
-              </div>
-              <div className="flex justify-between">
-                <button className="w-[93px] h-[35px] bg-tableUpdateBtn text-black rounded font-medium cursor-pointer hover:bg-opacity-50">
-                  Activate
-                </button>
-                <button className="w-[93px] h-[35px] bg-yellow-500 text-black rounded font-medium cursor-pointer hover:bg-opacity-50">
-                  Suspend
-                </button>
-                <button className="w-[93px] h-[35px] bg-tableDeleteBtn text-black rounded font-medium cursor-pointer hover:bg-opacity-50">
-                  Ban
-                </button>
-              </div>
+            <div className="flex flex-col gap-[10px]">
+              <label
+                htmlFor=""
+                className="font-semibold text-[14px] text-greyText"
+              >
+                Project Type
+              </label>
+              <select
+                name=""
+                id=""
+                className="bg-inputBox w-[347px] h-[37px] rounded focus:outline-none p-1"
+                value={projectType}
+                onChange={(e) => setProjectType(e.target.value)}
+              >
+                <option value="Personal">Personal</option>
+                <option value="Group">Group</option>
+              </select>
             </div>
+            {projectType === "Group" && (
+              <div className="flex flex-col gap-[10px]">
+                <label
+                  htmlFor=""
+                  className="font-semibold text-[14px] text-greyText"
+                >
+                  Select Collaborators
+                </label>
+                <div className="flex justify-between w-[347px] gap-3">
+                  <input
+                    type="text"
+                    value={collaboratorInput}
+                    onChange={(e) => setCollaboratorInput(e.target.value)}
+                    className="bg-inputBox flex-grow h-[37px] rounded focus:outline-none p-1"
+                  />
+                  <button
+                    type="submit"
+                    className="bg-primary hover:opacity-75 text-[14px] px-[6px] font-bold rounded"
+                  >
+                    Add Collaborator
+                  </button>
+                </div>
+
+                <ul className="bg-inputBox p-2 rounded ">
+                  {collaborators.map((collaborator, i) => (
+                    <li className="text-primary" key={i}>
+                      {collaborator}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </form>
