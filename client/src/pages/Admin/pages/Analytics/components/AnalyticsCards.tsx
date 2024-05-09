af9bf808c4d31d42a63144b14c400f9d668a817c
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-09 03:49:46 +0300

    linked and rendered to the analytics cards

--- client/src/pages/Admin/pages/Analytics/components/AnalyticsCards.tsx
+++ client/src/pages/Admin/pages/Analytics/components/AnalyticsCards.tsx
@@ -2,52 +2,67 @@ import postslogo from "../../../assets/icons/grey/Group 128.svg";
 import Musicians from "../../../assets/icons/grey/Vector (1).svg";
 import Moderators from "../../../assets/icons/grey/Vector (2).svg";
 import Tracks from "../../../assets/icons/grey/Group.svg";
+import { useAppSelector } from "../../../../../app/hooks";
 
 const AnalyticsCards = () => {
+  const posts = useAppSelector((state) => state.post.posts);
+  const users = useAppSelector((state) => state.users.user);
+  const tracks = useAppSelector((state) => state.track.tracks);
+  const projects = useAppSelector((state) => state.projects.project);
+
   return (
     <div className="mt-[26px] mb-[30px] mx-auto w-[1133px] h-full flex flex-wrap justify-start gap-[22px]">
       <div className="p-[20px] w-[350px] h-[210px] bg-background flex flex-col items-center justify-around rounded-xl">
         <div className="bg-backgroundPurple bg-opacity-60 p-3 rounded-full">
           <img src={postslogo} alt="" className="w-[30px] h-[30px]" />
         </div>
-
-        <h2 className=" font-bold text-[25px] text-tableRow mt-[6px]">15</h2>
+        <h2 className=" font-bold text-[25px] text-tableRow mt-[6px]">
+          {posts?.length}
+        </h2>
         <h2 className=" font-medium text-[20px] text-tableRow">
           Posts Created
         </h2>
       </div>
       <div className="p-[20px] w-[350px] h-[210px] bg-background flex flex-col items-center justify-around rounded-xl">
-        <div className="bg-tableBackground p-3 rounded-full">
+        <div className="bg-backgroundPurple p-3 rounded-full">
           <img src={Musicians} alt="" className="w-[30px] h-[30px]" />
         </div>
 
-        <h2 className=" font-bold text-[25px] text-tableRow mt-[6px]">15</h2>
+        <h2 className=" font-bold text-[25px] text-tableRow mt-[6px]">
+          {users?.filter((user) => user.role_id == 3).length}
+        </h2>
         <h2 className=" font-medium text-[20px] text-tableRow">
           Musicians Registered
         </h2>
       </div>
       <div className="p-[20px] w-[350px] h-[210px] bg-background flex flex-col items-center justify-around rounded-xl">
-        <div className="bg-tableBackground p-3 rounded-full">
+        <div className="bg-backgroundPurple p-3 rounded-full">
           <img src={Moderators} alt="" className="w-[30px] h-[30px]" />
         </div>
 
-        <h2 className=" font-bold text-[25px] text-tableRow mt-[6px]">15</h2>
+        <h2 className=" font-bold text-[25px] text-tableRow mt-[6px]">
+          {" "}
+          {users?.filter((user) => user.role_id == 2).length}
+        </h2>
         <h2 className=" font-medium text-[20px] text-tableRow">
-          Moderators Invited
+          Active Moderators
         </h2>
       </div>
       <div className="p-[20px] w-[350px] h-[210px] bg-background flex flex-col items-center justify-around rounded-xl">
-        <div className="bg-tableBackground p-3 rounded-full">
+        <div className="bg-backgroundPurple p-3 rounded-full">
           <img src={Tracks} alt="" className="w-[30px] h-[30px]" />
         </div>
 
-        <h2 className=" font-bold text-[25px] text-tableRow mt-[6px]">15</h2>
+        <h2 className=" font-bold text-[25px] text-tableRow mt-[6px]">
+          {" "}
+          {tracks?.length}
+        </h2>
         <h2 className=" font-medium text-[20px] text-tableRow">
           Tracks Produced
         </h2>
       </div>
       <div className="p-[20px] w-[350px] h-[210px] bg-background flex flex-col items-center justify-around rounded-xl">
-        <div className="bg-tableBackground p-3 rounded-full">
+        <div className="bg-backgroundPurple p-3 rounded-full">
           <img src={postslogo} alt="" className="w-[30px] h-[30px]" />
         </div>
 
@@ -57,21 +72,14 @@ const AnalyticsCards = () => {
         </h2>
       </div>
       <div className="p-[20px] w-[350px] h-[210px] bg-background flex flex-col items-center justify-around rounded-xl">
-        <div className="bg-tableBackground p-3 rounded-full">
+        <div className="bg-backgroundPurple p-3 rounded-full">
           <img src={postslogo} alt="" className="w-[30px] h-[30px]" />
         </div>
 
-        <h2 className=" font-bold text-[25px] text-tableRow mt-[6px]">15</h2>
-        <h2 className=" font-medium text-[20px] text-tableRow">
-          Projects Created
+        <h2 className=" font-bold text-[25px] text-tableRow mt-[6px]">
+          {" "}
+          {projects?.length}
         </h2>
-      </div>
-      <div className="p-[20px] w-[350px] h-[210px] bg-background flex flex-col items-center justify-around rounded-xl">
-        <div className="bg-tableBackground p-3 rounded-full">
-          <img src={postslogo} alt="" className="w-[30px] h-[30px]" />
-        </div>
-
-        <h2 className=" font-bold text-[25px] text-tableRow mt-[6px]">15</h2>
         <h2 className=" font-medium text-[20px] text-tableRow">
           Projects Created
         </h2>
