d83785d8c70ab312cfe936042fd6ef710981ad64
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-09 03:18:15 +0300

    created a projects api

---
+++ client/src/redux/projects/projectsApis.ts
@@ -0,0 +1,9 @@
+import { sendRequest } from "../../core/remote/request";
+
+export const fetchAllProjectsApi = async () => {
+  const res = await sendRequest("GET", `/projects`, "");
+  if (res.status == 200) {
+    return res.data.result;
+  }
+  return null;
+};
