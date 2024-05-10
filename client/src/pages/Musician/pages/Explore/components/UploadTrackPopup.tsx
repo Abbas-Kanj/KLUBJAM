5dd7d23842d4f9e5bd6f0a276691f7ebdf894d0e
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-10 12:59:32 +0300

    styling the upload track popup

--- client/src/pages/Musician/pages/Explore/components/UploadTrackPopup.tsx
+++ client/src/pages/Musician/pages/Explore/components/UploadTrackPopup.tsx
@@ -3,7 +3,54 @@ interface PostProps {
 }
 
 const UploadTrackPopup: React.FC<PostProps> = ({ setOpenUploadTrackPopup }) => {
-  return <div></div>;
+  return (
+    <div className="flex">
+      {/* <button onClick={createTrack}>send!</button> */}
+      <div className="flex flex-col w-[460px] items-center gap-[12px] p-[16px] mt-[20px]">
+        {/* <img src={image} /> */}
+        <input
+          id="image-upload"
+          type="file"
+          accept="image/*"
+          className="hidden"
+          //   onChange={handleImageUpload}
+        />
+        <label
+          htmlFor="image-upload"
+          className="bg-primary w-[220px] text-[16px] font-bold rounded-lg text-center p-[6px] cursor-pointer"
+        >
+          Select image to upload
+        </label>
+      </div>
+      <div className="flex flex-col w-[460px] items-center gap-[12px] p-[16px] mt-[20px]">
+        {/* <img src={image} /> */}
+        <input
+          id="audio-upload"
+          type="file"
+          accept="image/*"
+          className="hidden"
+          //   onChange={handleTrackUpload}
+        />
+        <label
+          htmlFor="audio-upload"
+          className="bg-primary w-[220px] text-[16px] font-bold rounded-lg text-center p-[6px] cursor-pointer"
+        >
+          Select track to upload
+        </label>
+      </div>
+      <div className="flex flex-col justify-between pl-[30px] border-l border-solid border-[#565656]">
+        <div className="flex items-center gap-[12px] align-center gap mt-[30px]">
+          <img
+            // src={user?.profile_picture}
+            alt=""
+            className="max-w-[40px] max-h-[40px]"
+          />
+          {/* <h5 className="text-[14px] font-medium">{user?.username}</h5> */}
+        </div>
+        {/* {error && <small className="text-red">{error}</small>} */}
+      </div>
+    </div>
+  );
 };
 
 export default UploadTrackPopup;
