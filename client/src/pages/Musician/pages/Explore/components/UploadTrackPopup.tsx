ab217386ed4f23244d2bed92307aa5ede00c11dd
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-10 12:57:51 +0300

    created an upload track popup

---
+++ client/src/pages/Musician/pages/Explore/components/UploadTrackPopup.tsx
@@ -0,0 +1,9 @@
+interface PostProps {
+  setOpenUploadTrackPopup: (open: boolean) => void;
+}
+
+const UploadTrackPopup: React.FC<PostProps> = ({ setOpenUploadTrackPopup }) => {
+  return <div></div>;
+};
+
+export default UploadTrackPopup;
