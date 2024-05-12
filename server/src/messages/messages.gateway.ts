ee980eeedfab6e182a7c571a48b40ecbe2eb953a
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-12 16:06:49 +0300

    created a gateway to create messages

--- server/src/messages/messages.gateway.ts
+++ server/src/messages/messages.gateway.ts
@@ -2,10 +2,11 @@ import {
   WebSocketGateway,
   SubscribeMessage,
   MessageBody,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
+import { Server } from 'socket.io';
 
 @WebSocketGateway({
   cors: {
@@ -13,15 +14,26 @@ import { UpdateMessageDto } from './dto/update-message.dto';
   },
 })
 export class MessagesGateway {
+  @WebSocketServer()
+  server: Server;
   constructor(private readonly messagesService: MessagesService) {}
 
   @SubscribeMessage('createMessage')
-  create(@MessageBody() createMessageDto: CreateMessageDto) {
-    return this.messagesService.create(createMessageDto);
+  async create(@MessageBody() createMessageDto: CreateMessageDto) {
+    const message = await this.messagesService.create(createMessageDto);
+
+    this.server.emit('message', message);
+    return message;
   }
 
   @SubscribeMessage('findAllMessages')
   findAll() {
     return this.messagesService.findAll();
   }
+
+  @SubscribeMessage('typing')
+  joinRoom() {}
+
+  @SubscribeMessage('typing')
+  async typing() {}
 }
