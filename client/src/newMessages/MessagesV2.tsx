524bedb46536dfce47b0d96c14e9106936f34767
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-12 20:40:44 +0300

    created a messgaing interface/ still displaying the sender name as undefined

--- client/src/newMessages/MessagesV2.tsx
+++ client/src/newMessages/MessagesV2.tsx
@@ -1,39 +1,40 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 const MessagesV2 = () => {
   const socket = io("http://localhost:3000");
 
-  const messages = useRef<any[]>([]);
-  const messageText = useRef<string>("");
-  const joined = useRef<boolean>(false);
-  const name = useRef<string>("");
-  const typingDisplay = useRef<string>("");
+  let timeout;
+  const [messages, setMessages] = useState<any[]>([]);
+  const [messageText, setMessageText] = useState<string>("");
+  const [joined, setJoined] = useState<boolean>(false);
+  const [name, setName] = useState("");
+  const [typingDisplay, setTypingDisplay] = useState<string>("");
 
   useEffect(() => {
     socket.emit("findAllMessages", {}, (res: any) => {
-      messages.current = res;
-      console.log(messages.current);
+      setMessages(res);
     });
 
     socket.on("message", (message) => {
-      messages.current.push(message);
+      setMessages((prevMessages) => [...prevMessages, message]);
     });
   }, []);
 
-  const join = () => {
-    socket.emit("join", { name: name.current }, () => {
-      joined.current = true;
+  const join = (name: string) => {
+    console.log("Name before join:", name);
+    socket.emit("join", { name: name }, () => {
+      setJoined(true);
     });
   };
 
-  const sendMessage = () => {
-    socket.emit("createMessage", { text: messageText.current }, () => {
-      messageText.current = "";
+  const sendMessage = (e: React.FormEvent) => {
+    e.preventDefault();
+    socket.emit("createMessage", { text: messageText }, () => {
+      setMessageText("");
     });
   };
 
-  let timeout;
   const emitTyping = () => {
     socket.emit("typing", { isTyping: true });
     timeout = setTimeout(() => {
@@ -43,9 +44,53 @@ const MessagesV2 = () => {
 
   return (
     <div className="flex justify-center items-center bg-black">
-      {messages.current.map((message, i) => (
-        <div key={i}>{` ${message.name}  ${message.text}`}</div>
-      ))}
+      {!joined ? (
+        <div className="flex flex-col w-screen">
+          <div>
+            <label>What's your name?</label>
+            <input
+              id="name"
+              type="text"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <button
+              onClick={() => {
+                join(name);
+              }}
+            >
+              join room
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col">
+          <div className="flex flex-col w-screen bg-greyText">
+            {messages.map((message, i) => (
+              <div key={i}>{`${message.name}  ${message.text}`}</div>
+            ))}
+          </div>
+          <form className="flex" onSubmit={sendMessage}>
+            <label className="font-semibold text-[14px] text-greyText">
+              Message
+            </label>
+            <input
+              id="message"
+              type="text"
+              className="bg-inputBox flex-grow h-[37px] rounded focus:outline-none p-1"
+              value={messageText}
+              onChange={(e) => setMessageText(e.target.value)}
+            />
+            <button
+              className="bg-primary hover:opacity-75 text-[14px] px-[6px] font-bold rounded"
+              type="submit"
+            >
+              Send message
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
