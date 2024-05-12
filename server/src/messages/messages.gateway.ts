42647fb35dec3c0cfd02cd22b7bca1bb067ee4e4
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-12 15:42:39 +0300

    setting up messages gateway v2

---
+++ server/src/messages/messages.gateway.ts
@@ -0,0 +1,27 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+} from '@nestjs/websockets';
+import { MessagesService } from './messages.service';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class MessagesGateway {
+  constructor(private readonly messagesService: MessagesService) {}
+
+  @SubscribeMessage('createMessage')
+  create(@MessageBody() createMessageDto: CreateMessageDto) {
+    return this.messagesService.create(createMessageDto);
+  }
+
+  @SubscribeMessage('findAllMessages')
+  findAll() {
+    return this.messagesService.findAll();
+  }
+}
