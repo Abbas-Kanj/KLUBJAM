c473485779f5762d7c4d26067490c43464e78be3
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-12 17:40:29 +0300

    created a sendMessage, join, typing functions to emit to the socket server

--- client/src/newMessages/MessagesV2.tsx
+++ client/src/newMessages/MessagesV2.tsx
@@ -5,13 +5,41 @@ const MessagesV2 = () => {
   const socket = io("http://localhost:3000");
 
   const messages = useRef<any[]>([]);
+  const messageText = useRef<string>("");
+  const joined = useRef<boolean>(false);
+  const name = useRef<string>("");
+  const typingDisplay = useRef<string>("");
 
   useEffect(() => {
     socket.emit("findAllMessages", {}, (res: any) => {
       messages.current = res;
       console.log(messages.current);
     });
+
+    socket.on("message", (message) => {
+      messages.current.push(message);
+    });
   }, []);
+
+  const join = () => {
+    socket.emit("join", { name: name.current }, () => {
+      joined.current = true;
+    });
+  };
+
+  const sendMessage = () => {
+    socket.emit("createMessage", { text: messageText.current }, () => {
+      messageText.current = "";
+    });
+  };
+
+  let timeout;
+  const emitTyping = () => {
+    socket.emit("typing", { isTyping: true });
+    timeout = setTimeout(() => {
+      socket.emit("typing", { isTyping: false });
+    }, 2000);
+  };
 
   return (
     <div className="flex justify-center items-center bg-black">
