451b8fd96e4ae8259cc0b5f33fee4eba777280f7
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-12 22:24:16 +0300

    created a group project popup

---
+++ client/src/pages/Musician/pages/Workspace/components/GroupProjectPopup.tsx
@@ -0,0 +1,13 @@
+interface ProjectProps {
+  setOpenGroupProjectPopup: (open: boolean) => void;
+  project: any;
+}
+
+const GroupProjectPopup: React.FC<ProjectProps> = ({
+  setOpenGroupProjectPopup,
+  project,
+}) => {
+  return <div></div>;
+};
+
+export default GroupProjectPopup;
