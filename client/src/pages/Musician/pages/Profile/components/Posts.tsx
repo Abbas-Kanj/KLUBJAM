e28eceb250a48b70ba59dbdab5996665afbf6726
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-12 21:00:36 +0300

    fixed styling to display videos and images

--- client/src/pages/Musician/pages/Profile/components/Posts.tsx
+++ client/src/pages/Musician/pages/Profile/components/Posts.tsx
@@ -16,16 +16,20 @@ const Posts = () => {
     }
   }, [auth_token, dispatch]);
   return (
-    <div className="flex flex-wrap justify-start gap-[14px] ml-[200px] mr-[200px] mb-[40px]">
+    <div className="flex flex-wrap justify-start gap-[14px] ml-[190px] mr-[190px] mb-[40px]">
       {userPosts.map((post, i) => (
         <div key={i}>
           {post.post_picture.endsWith(".mp4") ? (
             <video
               src={`http://127.0.0.1:3000${post.post_picture}`}
-              className="max-w-[315px] max-h-[440px]"
+              className="max-w-[297px] max-h-[297px] min-h-[297px] min-w-[297px]"
             />
           ) : (
-            <img src={`http://127.0.0.1:3000${post.post_picture}`} alt="" />
+            <img
+              src={`http://127.0.0.1:3000${post.post_picture}`}
+              alt=""
+              className="max-w-[297px] max-h-[297px] min-h-[297px] min-w-[297px]"
+            />
           )}
         </div>
       ))}
