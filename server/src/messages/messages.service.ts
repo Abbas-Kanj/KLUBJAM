26b8d57684c3ed3a8be1a618055cb6efc05ebe46
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-12 16:19:29 +0300

    created an get client by name service, and defined the identify function used to identify connected users

--- server/src/messages/messages.service.ts
+++ server/src/messages/messages.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
 
 @Injectable()
 export class MessagesService {
+  messages: Message[] = [{ name: 'marius', text: 'hey' }];
+  clientToUser = {};
+  identify(name: string, clientId: string) {
+    this.clientToUser[clientId] = name;
+    return Object.values(this.clientToUser);
+  }
+  getClientName(clientId: string) {
+    return this.clientToUser[clientId];
+  }
+
   create(createMessageDto: CreateMessageDto) {
-    return 'This action adds a new message';
+    const message = { ...createMessageDto };
+    this.messages.push(message);
+    return message;
   }
 
   findAll() {
