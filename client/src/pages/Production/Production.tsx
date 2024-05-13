221db787285b0a6bc3f8d0f1b6919adc77f39bdd
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-13 16:36:23 +0300

    styled project info container

--- client/src/pages/Production/Production.tsx
+++ client/src/pages/Production/Production.tsx
@@ -9,6 +9,7 @@ import volume from "./assets/icons/volume-05.svg";
 import settings from "./assets/icons/settings (1).svg";
 import download from "./assets/icons/download-02.svg";
 import elipse from "./assets/icons/Ellipse 41.svg";
+import circle from "../assets/Workspace/icons/circle.svg";
 
 const Production = () => {
   return (
@@ -130,7 +131,29 @@ const Production = () => {
             </div>
           </div>
           <div className="flex flex-col gap-2">
-            <div className="w-[270px] h-[240px] border-[2px] border-solid rounded-[5px] border-primary"></div>
+            <div className="w-[270px] h-[240px] border-[2px] border-solid rounded-[5px] border-primary p-[14px]">
+              <div className=" text-center items-center justify-between flex gap-[14px]">
+                <h1 className="font-bold text-[20px] text-primary">
+                  Vibra Verse
+                </h1>
+                <h2 className=" w-[58px] h-[24px] font-bold text-[10px] border-[2px] border-solid border-greyText rounded-lg p-[4px] text-greyText cursor-pointer hover:text-primary hover:border-primary">
+                  Private
+                </h2>
+              </div>
+              <div className="flex items-center mt-[9px] mb-[9px]">
+                <img src={circle} alt="" className="mr-[6px]" />
+                <h2 className="mr-[28px] font-bold text-[12px] text-greyText">
+                  R&B
+                </h2>
+                <h2 className="text-[13px] text-greyText">
+                  Updated 6 hours ago
+                </h2>
+              </div>
+              <p className="text-[14px] text-greyText">
+                This track features vibrant synths, a groovy bassline, and
+                dynamic drums, creating a lively and energetic vibe
+              </p>
+            </div>
             <div className="w-[270px] h-[370px] border-[2px] border-solid rounded-[5px] border-primary flex flex-col justify-between">
               <div></div>
               <div className="flex flex-col">
