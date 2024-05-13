3f6023e8a286ee17d361ca1258e600d21fb270b5
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-13 16:02:16 +0300

    styled buttons in production

--- client/src/pages/Production/Production.tsx
+++ client/src/pages/Production/Production.tsx
@@ -1,12 +1,21 @@
+import globe from "./assets/icons/globe.svg";
+import save from "./assets/icons/save-02.svg";
+
 const Production = () => {
   return (
     <div className="flex justify-center bg-background w-screen h-screen px-[12px] py-[20px]">
       <div>
-        <div className="flex">
+        <div className="flex gap-[10px]">
           <div className="w-[1210px] h-[75px] border-[2px] border-solid rounded-[5px] border-primary"></div>
-          <div className="flex">
-            <button></button>
-            <button></button>
+          <div className="flex justify-between items-start gap-[9px]">
+            <div className="flex items-center justify-center w-[120px] h-[46px] bg-transparent cursor-pointer pt-[6px] pb-[6px] pl-[15px] pr-[15px] gap-[5px] hover:opacity-70 rounded-md border-[2px] border-solid border-primary">
+              <img src={save} alt="" className="w-[28px] h-[28px]" />
+              <h2 className="font-bold text-[14px]">Save</h2>
+            </div>
+            <div className="flex items-center justify-center w-[120px] h-[46px] bg-primary cursor-pointer pt-[6px] pb-[6px] pl-[15px] pr-[15px] gap-[5px] hover:opacity-70 rounded-md">
+              <img src={globe} alt="" className="w-[28px] h-[28px]" />
+              <h2 className="font-bold text-[14px]">Publish</h2>
+            </div>
           </div>
         </div>
         <div className="flex mt-[9px] gap-[6px]">
