4d9790893ba19e540f5c6f6382684fef4cf12fc4
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-13 15:44:37 +0300

    styled chat in production

--- client/src/pages/Production/Production.tsx
+++ client/src/pages/Production/Production.tsx
@@ -14,8 +14,23 @@ const Production = () => {
           <div className="w-[156px] h-[620px] border-[2px] border-solid rounded-[5px] border-primary"></div>
           <div className="w-[854px] h-[620px] border-[2px] border-solid rounded-[5px] border-primary"></div>
           <div className="flex flex-col gap-2">
-            <div className="w-[266px] h-[280px] border-[2px] border-solid rounded-[5px] border-primary"></div>
-            <div className="w-[266px] h-[330px] border-[2px] border-solid rounded-[5px] border-primary"></div>
+            <div className="w-[270px] h-[240px] border-[2px] border-solid rounded-[5px] border-primary"></div>
+            <div className="w-[270px] h-[370px] border-[2px] border-solid rounded-[5px] border-primary flex flex-col justify-between">
+              <div></div>
+              <div className="flex flex-col">
+                <div className="border border-solid border-primary"></div>
+                <div className="px-[5px] py-[9px] flex justify-between gap-1">
+                  <input
+                    type="text"
+                    placeholder="Type a message..."
+                    className="border-[2px] border-solid rounded-[5px] border-primary w-[192px] h-[30px] bg-transparent p-1"
+                  />
+                  <button className="bg-primary rounded-[5px] flex-grow font-bold">
+                    Send
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
