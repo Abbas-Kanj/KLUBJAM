d0a1545144e35a88151543d74f69283bfa9fd24c
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-14 14:17:31 +0300

    created a controller button component for the audio player

---
+++ client/src/pages/Musician/components/ControlsToggleButton.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Button from "@material-ui/core/Button";
+
+function ControlsToggleButton(props: any) {
+  let ButtonOne = props.defaultIcon;
+  let ButtonTwo = props.changeIcon;
+  const [buttonType, setButton] = useState(false);
+
+  function handleChange() {
+    if (props.type === "prev" || props.type === "next") {
+      setButton(true);
+      props.onClicked(props.type, true);
+    } else {
+      setButton(!buttonType);
+      props.onClicked(props.type, !buttonType);
+    }
+  }
+
+  return (
+    <Button style={props.style} onClick={handleChange} className={props.type}>
+      {!buttonType ? ButtonOne : ButtonTwo}
+    </Button>
+  );
+}
+
+export default ControlsToggleButton;
