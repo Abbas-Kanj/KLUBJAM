49eeef3aad52953865c34b04e788a309698b2d84
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-14 22:20:29 +0300

    created a jambox page

--- client/src/pages/Musician/pages/JamBox/index.tsx
+++ client/src/pages/Musician/pages/JamBox/index.tsx
@@ -1,7 +1,63 @@
-import React from "react";
-
+import bgHeader from "../../../assets/Navbar/images/Rectangle 75.png";
+import defaultLogo from "../../../assets/Navbar/images/user-profile-circle.svg";
+import { useState } from "react";
+import Magenta from "./components/Magenta";
+import JamBoxAi from "./components/JamBox";
 const Jambox = () => {
-  return <div></div>;
+  const [isVisible1, setIsVisible1] = useState(true);
+  const [isVisible2, setIsVisible2] = useState(false);
+
+  const handleClick = (componentNumber: number) => {
+    if (componentNumber === 1) {
+      setIsVisible1(!isVisible1);
+    } else if (componentNumber === 2) {
+      setIsVisible2(!isVisible2);
+    }
+  };
+  return (
+    <div className="flex flex-col w-[1316px] bg-backgroundDark overflow-x-hidden overflow-scroll">
+      <div className="h-[180px] flex relative">
+        <img
+          src={bgHeader}
+          alt=""
+          className="w-full h-full object-center absolute inset-0"
+        />
+        <img
+          src={defaultLogo}
+          alt=""
+          className="absolute top-3 right-3 cursor-pointer"
+        />
+        <h1 className="relative mt-[67px] mb-[67px] ml-[35px] font-semibold text-[38px]">
+          Workspace
+        </h1>
+
+        <div className="flex absolute bottom-[13px] left-9 gap-[41px]">
+          <h2
+            className="text-[13px] font-bold hover:text-primary cursor-pointer text-greyText"
+            onClick={() => {
+              handleClick(1);
+              setIsVisible2(false);
+            }}
+          >
+            JamBox
+          </h2>
+          <h2
+            className="text-[13px] font-bold hover:text-primary cursor-pointer text-greyText"
+            onClick={() => {
+              handleClick(2);
+              setIsVisible1(false);
+            }}
+          >
+            Generated Jam
+          </h2>
+        </div>
+      </div>
+      <div className="flex flex-col relative items-center">
+        {isVisible1 && <JamBoxAi />}
+        {isVisible2 && <Magenta />}
+      </div>
+    </div>
+  );
 };
 
 export default Jambox;
