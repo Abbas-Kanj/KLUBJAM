dcdde3bd7503a8eb9531b975f92c16b857414f56
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-15 00:45:43 +0300

    added controlls to change the bpm for the step sequencer

--- client/src/tone/step_sequencer/StepSequencer.tsx
+++ client/src/tone/step_sequencer/StepSequencer.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface SequencerProps {
   setOpenStepSequencer: (open: boolean) => void;
 }
 
 const StepSequencer: React.FC<SequencerProps> = ({ setOpenStepSequencer }) => {
+  const notes = Array(64).fill("");
+  const [bpm, setBpm] = useState(120);
+
+  const handleBpmChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBpm(parseInt(event.target.value));
+  };
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-50">
       <p
@@ -13,9 +20,25 @@ const StepSequencer: React.FC<SequencerProps> = ({ setOpenStepSequencer }) => {
       >
         X
       </p>
-      <div className="flex items-center w-[1000px] h-fit rounded-xl bg-background">
-        <div className="sequencer grid grid-cols-16 gap-5 w-full">
-          <div className="note bg-gray-300 text-xs md:text-base w-12 h-12 md:w-16 md:h-16 border border-gray-300 rounded-lg flex justify-center items-center"></div>
+      <div className=" items-center w-[1000px] h-fit rounded-xl bg-background">
+        <div className="bpm-controls">
+          <label htmlFor="bpm">{bpm}BPM</label>
+          <input
+            type="range"
+            min="60"
+            max="240"
+            value={bpm}
+            onChange={handleBpmChange}
+          />
+          <button>Play</button>
+          <button>Stop</button>
+        </div>
+        <div className="sequencer">
+          {notes.map((note, i) => (
+            <div key={i} className={`note ${i == 0 ? "active" : ""}`}>
+              {note}
+            </div>
+          ))}
         </div>
       </div>
     </div>
