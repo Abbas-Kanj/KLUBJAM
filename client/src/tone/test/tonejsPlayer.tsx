bd0fe425488460c2b5e08040a22cb88dd5cf822a
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-15 19:39:18 +0300

    created an tone transport scheduale repeat

--- client/src/tone/test/tonejsPlayer.tsx
+++ client/src/tone/test/tonejsPlayer.tsx
@@ -3,19 +3,26 @@ import * as Tone from "tone";
 const TonejsPLayer = () => {
   const synth = new Tone.Synth();
   const audioContext = Tone.Context;
+  synth.toDestination();
+
   const notes = "CDEFGAB".split("").map((n) => `${n}4`);
   let note = 0;
-  synth.toDestination();
   Tone.Transport.scheduleRepeat((time) => {
-    synth.triggerAttack(notes[note % notes.length]);
+    if (note === 0) {
+    }
+    if (note > notes.length) {
+      synth.triggerRelease(time);
+      Tone.Transport.stop();
+    } else synth.triggerAttack(notes[note], time);
     note++;
   }, "4n");
-  // Tone.Transport.start();
+  Tone.Transport.start();
 
   return (
     <div className="flex justify-center items-center mt-10 bg-slate-500 w-screen h-screen">
       <div className="bg-zinc-950 w-fit">
         <audio src=""></audio>
+        <button onClick={() => {}}>start</button>
       </div>
     </div>
   );
