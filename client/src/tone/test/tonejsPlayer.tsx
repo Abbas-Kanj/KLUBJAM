ac07c31de83f6575f8820b50239896dd5fd84b6f
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-05-15 23:04:44 +0300

    recorded first synth and downloaded successfuly

--- client/src/tone/test/tonejsPlayer.tsx
+++ client/src/tone/test/tonejsPlayer.tsx
@@ -1,18 +1,25 @@
 import * as Tone from "tone";
 
 const TonejsPLayer = () => {
-  const synth = new Tone.Synth();
-  const audioContext = Tone.Context;
+  const recorder = new Tone.Recorder();
+  const synth = new Tone.Synth().connect(recorder);
   synth.toDestination();
 
   const notes = "CDEFGAB".split("").map((n) => `${n}4`);
   let note = 0;
-  Tone.Transport.scheduleRepeat((time) => {
+  Tone.Transport.scheduleRepeat(async (time) => {
     if (note === 0) {
+      recorder.start();
     }
     if (note > notes.length) {
       synth.triggerRelease(time);
       Tone.Transport.stop();
+      const recording = await recorder.stop();
+      const url = URL.createObjectURL(recording);
+      const anchor = document.createElement("a");
+      anchor.download = "recording.webm";
+      anchor.href = url;
+      anchor.click();
     } else synth.triggerAttack(notes[note], time);
     note++;
   }, "4n");
@@ -22,7 +29,13 @@ const TonejsPLayer = () => {
     <div className="flex justify-center items-center mt-10 bg-slate-500 w-screen h-screen">
       <div className="bg-zinc-950 w-fit">
         <audio src=""></audio>
-        <button onClick={() => {}}>start</button>
+        <button
+          onClick={() => {
+            Tone.start();
+          }}
+        >
+          start
+        </button>
       </div>
     </div>
   );
