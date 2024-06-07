bd8f0fa4c62ee98bbcc9411dfd8b07823ac7ca01
Abbas <abbas_kanj@hotmail.com>
2024-06-07 17:31:42 +0300

    created a global exception filter file and linked it to the main file

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import { join } from 'path';
+import { AllExceptionsFilter } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,7 @@ async function bootstrap() {
   };
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   app.enableCors(cors);
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3000);
 }
 bootstrap();
