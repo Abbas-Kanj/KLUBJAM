4591754b1aede3e6397eade428664044fc7c05b8
Abbas <abbas_kanj@hotmail.com>
2024-06-18 23:09:04 +0300

    added nav links and mapped them

--- client/src/pages/Musician/components/Sidebar.tsx
+++ client/src/pages/Musician/components/Sidebar.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import Logo from "../../../assets/icons/logo.svg";
+import { MdOutlineHome } from "react-icons/md";
+import { MdOutlineDashboard } from "react-icons/md";
+import { MdOutlineExplore } from "react-icons/md";
+import { MdOutlinePlaylistPlay } from "react-icons/md";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { MdOutlineSettings } from "react-icons/md";
+import { MdOutlineSmartToy } from "react-icons/md";
+import { MdOutlineLogout } from "react-icons/md";
+import Cookies from "universal-cookie";
 
 import { NavLink, useNavigate } from "react-router-dom";
 interface LayoutProps {
@@ -8,6 +17,16 @@ interface LayoutProps {
 
 const Sidebar: React.FC<LayoutProps> = ({ children }) => {
   const navigate = useNavigate();
+
+  const navigation = [
+    { name: "Home", icon: MdOutlineHome, href: "../Home" },
+    { name: "Workspace", icon: MdOutlineDashboard, href: "../Workspace" },
+    { name: "Explore", icon: MdOutlineExplore, href: "../Explore" },
+    { name: "Jambox", icon: MdOutlineSmartToy, href: "../Jambox" },
+    { name: "Playlists", icon: MdOutlinePlaylistPlay, href: "../Playlists" },
+    { name: "Profile", icon: MdOutlineAccountCircle, href: "../Profile" },
+    { name: "Settings", icon: MdOutlineSettings, href: "../Account" },
+  ];
 
   return (
     <div className="drawer lg:drawer-open gap-1">
@@ -32,6 +51,34 @@ const Sidebar: React.FC<LayoutProps> = ({ children }) => {
             <div className="flex items-center mb-16 mt-9 gap-[19px]">
               <img src={Logo} alt="" />
               <h1 className="font-semibold text-[22px]">KLUBJAM</h1>
+            </div>
+            <div className="flex flex-col gap-2">
+              {navigation.map((nav, i) => (
+                <NavLink
+                  key={i}
+                  to={nav.href}
+                  className={({ isActive }) => {
+                    return (
+                      "flex gap-3 items-center font-semibold p-1 " +
+                      (!isActive
+                        ? "text-greyText hover:bg-primary hover:bg-opacity-40 hover:rounded-lg -ml-4 pl-4"
+                        : "text-primary hover:bg-primary hover:bg-opacity-40 hover:rounded-lg border-l-4 border-primary border-solid rounded-l-sm -ml-4 pl-4")
+                    );
+                  }}
+                >
+                  {({ isActive }: { isActive: boolean }) => (
+                    <>
+                      <nav.icon
+                        className={
+                          "size-7 " +
+                          (!isActive ? "fill-greyText" : "fill-primary")
+                        }
+                      />
+                      {nav.name}
+                    </>
+                  )}
+                </NavLink>
+              ))}
             </div>
           </div>
         </ul>
