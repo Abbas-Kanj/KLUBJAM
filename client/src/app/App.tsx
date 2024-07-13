da96496d19a30c50723a1f407bcda20e338d565e
Abbas <abbas_kanj@hotmail.com>
2024-07-13 19:27:50 +0300

    created notfound page

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import store, { persistor } from "./store";
 import { Provider } from "react-redux";
 import Musician from "../pages/Musician";
-import Landing from "../pages/Landing";
 import Admin from "../pages/Admin";
 import Moderator from "../pages/Moderator";
 import { PersistGate } from "redux-persist/integration/react";
@@ -11,7 +10,8 @@ import ProtectedModerator from "../core/routes/ProtectedModerator";
 import ProtectedUser from "../core/routes/ProtectedUser";
 import { Toaster } from "react-hot-toast";
 import Production from "../pages/Production/Production";
-import UiTesting from "../test";
+import LandingPage from "../pages/Landing";
+import NotFoundPage from "../pages/NotFoundPage";
 
 function App() {
   return (
@@ -29,8 +29,8 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/Production" element={<Production />}></Route>
-            <Route path="/" element={<Landing />}></Route>
-            <Route path="/UiTest" element={<UiTesting />}></Route>
+            <Route path="/*" element={<NotFoundPage />}></Route>
+            <Route path="/" element={<LandingPage />}></Route>
             <Route element={<ProtectedUser />}>
               <Route path="/Musician/*" element={<Musician />} />
             </Route>
